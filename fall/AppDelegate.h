1577db798f2f66e51c16906ac463ecf87446fc65
古家　佑麻　　　　　　　　 <yumafuruya@furuyayuuasa-no-MacBook-Air.local>
2014-06-24 16:46:40 +0900

    Initial Commit

---
+++ fall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  fall
+//
+//  Created by 古家　佑麻　　　　　　　　 on 2014/06/24.
+//  Copyright (c) 2014年 yumafuruya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fall/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  fall
+//
+//  Created by 古家　佑麻　　　　　　　　 on 2014/06/24.
+//  Copyright (c) 2014年 yumafuruya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
